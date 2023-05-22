dbe59bd4246b0b9ffcbdc6cd5c95b18c4838dcca
Munazar-99 <munazar.ali.001@gmail.com>
2023-05-22 22:26:56 +0200

    adds more tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -51,3 +51,21 @@ it('returns single puppy through Id', async () => {
     expect(response.body.singleResult.errors).toBeUndefined();
     expect(response.body.singleResult.data?.getPuppy).toHaveProperty('name', 'Alfonsa');
 });
+it('adds a puppy to te database', async () => {
+
+    const response = await testServer.executeOperation({
+        query: `mutation {
+            addPuppy (inputBody:  {
+                  id: 11,
+                  breed:"Siberian husky",
+                  name:"sheldon",
+                  img: "https://images.unsplash.com/photo-1617895153857-82fe79adfcd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+                  DOB: "2 April 2001"
+              })
+          }`
+    });
+    console.log('munazar', response.body)
+    assert(response.body.kind === 'single');
+    expect(response.body.singleResult.errors).toBeUndefined();
+    expect(response.body.singleResult.data?.getPuppy).toHaveProperty('name', 'Alfonsa');
+});
